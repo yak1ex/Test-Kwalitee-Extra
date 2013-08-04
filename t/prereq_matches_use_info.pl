b14a1b8422d7d9171d0c7b4a2d4695fa308d825d
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-04 21:06:52 +0900

    prereq_matches_use and build_prereq_matches_use are no longer included in Module::CPANTS::Analyse (close gh-8).

--- t/prereq_matches_use_info.pl
+++ t/prereq_matches_use_info.pl
@@ -7,5 +7,9 @@ foreach my $val (@$ref) {
 	($error, $remedy) = @{$val}{qw(error remedy)} if $val->{name} eq 'prereq_matches_use';
 	($berror, $bremedy) = @{$val}{qw(error remedy)} if $val->{name} eq 'build_prereq_matches_use';
 }
+$error   ||= q{This distribution uses a module or a dist that's not listed as a prerequisite.};
+$remedy  ||= q{List all used modules in META.yml requires};
+$berror  ||= q{This distribution uses a module or a dist in it's test suite that's not listed as a build prerequisite.};
+$bremedy ||= q{List all modules used in the test suite in META.yml build_requires};
 
 return ($error, $remedy, $berror, $bremedy);
